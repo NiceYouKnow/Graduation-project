fa732cd83c73042c9b2d05d90f9c32a2c4de9f3b
范云飞 <34054835+NiceYouKnow@users.noreply.github.com>
2020-04-26 18:19:44 +0800

    注册bug修复
    修复查询参数过多导致查询结果不正确的问题

--- src/main/java/com/example/demo/mapper/RegisterMapper.java
+++ src/main/java/com/example/demo/mapper/RegisterMapper.java
@@ -14,7 +14,7 @@ public interface RegisterMapper {
 
  
 
- @Select("SELECT count(*) FROM TB_USER WHERE EMAIL = #{email} and PASSWORD = #{password}") //查询数据库
+ @Select("SELECT count(*) FROM TB_USER WHERE EMAIL = #{email}") //查询数据库
  Integer select(Register register);
  @Insert(value="INSERT INTO TB_USER (EMAIL,USERNAME,PASSWORD,SEX,BIRTHDAY,AGE) VALUES(#{email},#{username},#{password},#{sex},#{birthday},#{age})")//插入数据库
  void insert(Register register);
